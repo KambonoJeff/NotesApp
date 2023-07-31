32c4d4f1c31aab96c1ac70a3a9330e3267b6568a
KAMBONO <kambonoj@gmail.com>
2023-07-31 21:04:25 +0300

    python testing

--- python/gp.py
+++ python/gp.py
@@ -1,9 +1,13 @@
 import subprocess
+import random
+import string
+
 
 # Replace "your_command_here" with the actual command you want to run in the cmd.
-cmd_command = "git add ."
+cmd_command = "git add gp.py"
 cmd_command1 = " git commit -a -m \"python testing\" "
 cmd_command2 = "git push -u origin"
+
 def deploy():
     
     completed_process = subprocess.run(cmd_command, shell=True, check=True, text=True)
@@ -27,10 +31,54 @@ def deploy():
     else:
 
         print(" THIRD NOT COMPLETED!!!!")
+        print("SUCCESSFULLY PUSHED ALL FILES :")
+
+def get_user_input():
+    return input("ENTER THE NUMBER OF COMMITS: ")
+
+def text_generator_a():
+    with open("test.txt", "a") as file:
+        random_string = ' '.join(random.choices(string.ascii_letters, k=20))
+        print(random_string)
+        file.write(random_string)
+def text_generator_w():
+    with open("test.txt", "w") as file:
+        random_string = ' \n'.join(random.choices(string.ascii_letters, k=20))
+        print(random_string)
+        file.write(random_string)
+
+def randomFunc():
+    option = [text_generator_a,text_generator_w]
+    executeThis = random.choice(option)
+    executeThis()
+
+def formation():
+    text_generator_a()
+    deploy()
+
+def rounds():
+    userInput = get_user_input()
+    try:
+        number=int(userInput)
+        ######################################
+
+        ######################################
+        print("THE USER INPUT IS :" , number)
+        if number > 20:
+            print("The number is greater than 20 enter less commits")
+            get_user_input()
+        for _ in range(number):
+            formation()
+
+    except ValueError:
+        print("NOT AN INTEGER!!!")
+
 
 try:
-    deploy()
-    print("Command output:")
+    rounds()
+    
+   
+
 except subprocess.CalledProcessError as e:
     print(f"Command failed with exit code: {e.returncode}")
     print(f"Error message: {e.stderr}")
